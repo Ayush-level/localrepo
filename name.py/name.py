0c7ccf634a6461a043b11c31e6c647411b7bc841
Ayush-level <rishimahakud3@gmail.com>
2025-06-03 09:48:29 +0530

    new changes

--- name.py/name.py
+++ name.py/name.py
@@ -1,1 +1,9 @@
 print("Hello, World!")
+a=int(input("Enter a number: "))
+if a > 0:
+    print("Positive number")
+elif a == 0:
+    print("Zero")
+else:
+    print("Negative number")
+# Compare this snippet from python.py:
