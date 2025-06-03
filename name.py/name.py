83599d3bed18e3f8dd7cb00c7e471bece79b4a02
Ayush-level <rishimahakud3@gmail.com>
2025-06-03 09:44:00 +0530

    changes

--- name.py/name.py
+++ name.py/name.py
@@ -1,1 +1,8 @@
 print("Hello, World!")
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+print("The sum is:", a + b)
+
+c=int(input("Enter a number: "))
+d=int(input("Enter another number: "))
+print("The sum is:", c - d)
